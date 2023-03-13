e7ceff49ebef64839f4062861ed860cd6e715aaf
michal-boop <majzermichal@gmail.com>
2023-03-13 16:40:45 +0100

    create a blank html index page and script file linked to it containing pseudocode for coding rock-paper-scissors game

---
+++ myscripts.js
@@ -0,0 +1,6 @@
+//create a variable for users choice of weapon
+//create a variable to store copmuters choice of weapon
+//get users input to determine his variables value, make it case insensitive
+//use a function to randomly assign weapon to computer's variable
+//use conditional statements to determine the winner of a round
+//print the message announcing the winner and weapons of choice
