6ed711a1c1b9509999186df3ec06091d70d61ff9
michal-boop <majzermichal@gmail.com>
2023-03-13 21:41:14 +0100

    turn pseudocode into working code, allowing to play one round of the game using console

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,40 @@
-//create a variable for users choice of weapon
-//create a variable to store copmuters choice of weapon
-//get users input to determine his variables value, make it case insensitive
-//use a function to randomly assign weapon to computer's variable
-//use conditional statements to determine the winner of a round
-//print the message announcing the winner and weapons of choice
+let userChoice = prompt("Choose your weapon: Rock, Paper or Scissors?", "Rock").toLowerCase();//create a variable for user's choice of weapon, make it case insensitive
+console.log(userChoice)
+let computerChoice = getComputerChoice();//create a variable to store computer's choice of weapon
+console.log(computerChoice)
+function getComputerChoice() {
+    let computerWeapon
+    let x = Math.ceil(Math.random()*9);
+    console.log(x);
+    if (x<=3) {
+        computerWeapon = "rock";
+    }
+    else if (x<=6) {
+        computerWeapon = "paper";
+    }
+    else {
+        computerWeapon = "scissors";
+    }
+    console.log(computerWeapon)
+    return computerWeapon;  
+}//use a function to randomly assign weapon to computer's variable
+let userScore = 0
+console.log(userScore)
+let computerScore = 0
+console.log(computerScore)
+function playRound (userChoice, computerChoice) {
+    if (userChoice === computerChoice) { //use conditional statements to determine the winner of a round
+        alert (`You chose ${userChoice}, computer chose ${computerChoice}, it's a tie!`) //print the message announcing the winner and weapons of choice
+    }
+    else if ((userChoice === "rock" && computerChoice === "paper")||(userChoice === "paper" && computerChoice === "scissors")||(userChoice === "scissors" && computerChoice === "rock")) {
+        alert (`You chose ${userChoice}, computer chose ${computerChoice}, computer scores!`)
+        computerScore++
+        alert (`User: ${userScore} vs Computer: ${computerScore}`)
+    }
+    else {
+        alert (`You chose ${userChoice}, computer chose ${computerChoice}, user scores!`)
+        userScore++
+        alert (`User: ${userScore} vs Computer: ${computerScore}`)
+    }
+}
+playRound(userChoice, computerChoice)//play one round of the game
