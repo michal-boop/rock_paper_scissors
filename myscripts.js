54b08fad3d64ba44091f03e5cbeb1017bc2898da
michal-boop <majzermichal@gmail.com>
2023-03-22 20:30:41 +0100

    fix trophy size, clean up the code and add comments to the code

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,7 @@
 let userChoice //create a variable for user's choice of weapon
 let computerChoice//create a variable to store computer's choice of weapon
 
-function getComputerChoice() { //use a function to randomly assign weapon to computer's variable
+function getComputerChoice() { //use a function to randomly assign weapon to computer's variable using Math.random function
     let computerWeapon
     let x = Math.ceil(Math.random()*9);
     console.log(x);
@@ -31,53 +31,30 @@ function playRound (userChoice, computerChoice) {
         document.getElementById("message").innerHTML=`You chose ${userChoice}, Computer chose ${computerChoice}, Computer scores!`;
         computerScore++
     }
-    else /*if ((userChoice === "rock" && computerChoice === "scissors")||(userChoice === "paper" && computerChoice === "rock")||(userChoice === "scissors" && computerChoice === "paper"))*/{
-        document.getElementById("message").innerHTML=`You chose ${userChoice}, Computer chose ${computerChoice}, User scores!`;
+    else /*if ((userChoice === "rock" && computerChoice === "scissors")||(userChoice === "paper" && computerChoice === "rock")||(userChoice === "scissors" && computerChoice === "paper"))*/{ //no need to specify third option since typos are no longer an option
+        document.getElementById("message").innerHTML=`You chose ${userChoice}, Computer chose ${computerChoice}, User scores!`; //display results of the current round
         userScore++
     }
-    document.getElementById("user_score").innerHTML=`${userScore}`;
+    document.getElementById("user_score").innerHTML=`${userScore}`; //display value of scores in the scoreboard table
     document.getElementById("computer_score").innerHTML=`${computerScore}`;
 }
 
-const btn = document.querySelectorAll('button');
-btn.forEach((button) => {
-    button.addEventListener('click', () => {
-        userChoice = `${button.id}`;
-        computerChoice = getComputerChoice();
-        playRound(userChoice, computerChoice);
-        checkWin(userScore, computerScore);
+const btn = document.querySelectorAll('button'); //target all button elements
+btn.forEach((button) => { 
+    button.addEventListener('click', () => { //following functions are all triggered by clicking on buttons
+        userChoice = `${button.id}`; //but specific value of buttons is assigned as usetChoice
+        computerChoice = getComputerChoice(); //computerChoice is assigned after the user
+        playRound(userChoice, computerChoice); //function playing a single round is triggered
+        checkWin(userScore, computerScore); //conditions for endgame are checked after every round
     });
 });
 
-/*const btnRock = document.querySelector('#rock');
-btnRock.addEventListener('click', () => {
-    userChoice = "rock";
-    computerChoice = getComputerChoice();
-    playRound(userChoice, computerChoice);
-    checkWin(userScore, computerScore);
-});
 
-const btnPaper = document.querySelector('#paper');
-btnPaper.addEventListener('click', () => {
-    userChoice = "paper";
-    computerChoice = getComputerChoice();
-    playRound(userChoice, computerChoice);
-    checkWin(userScore, computerScore);
-});
-
-const btnScissors = document.querySelector('#scissors');
-btnScissors.addEventListener('click', () => {
-    userChoice = "scissors";
-    computerChoice = getComputerChoice();
-    playRound(userChoice, computerChoice);
-    checkWin(userScore, computerScore);
-}); */
-
-function checkWin (userScore, computerScore) {
+function checkWin (userScore, computerScore) { //this function checks conditions for endgame
     if (userScore===5) {
-        setTimeout(() => {document.getElementById("message").innerHTML="User wins!";}, "1000"); //display message for the winner
-        setTimeout(() => {showImage();}, "2000");
-        setTimeout(() => {location.reload();}, "3000");
+        setTimeout(() => {document.getElementById("message").innerHTML="User wins!";}, "1000"); //display message for the winner with a slight delay so that a message with results of the last round still runs 
+        setTimeout(() => {showImage();}, "2000"); //displays trophy for a user
+        setTimeout(() => {location.reload();}, "3000"); //reloads the page erasing the scores
     }
     else if (computerScore===5) {
         setTimeout(() => {document.getElementById("message").innerHTML="Computer wins!";}, "1000");
@@ -85,10 +62,11 @@ function checkWin (userScore, computerScore) {
     } 
 }
 
-function showImage () {
+function showImage () { //function responsible for showing a trophy in front of everything
     let img = document.createElement("img");
     img.src = "trophy.png";
     img.alt = "Trophy Cup";
+    img.style.maxHeight = "80%";
     img.style.zIndex = "1";
     img.style.position = "fixed";
     img.style.top = "50%";
@@ -96,7 +74,7 @@ function showImage () {
     img.style.left = "50%";
     img.style.marginLeft = "-343px";
     img.style.filter = "drop-shadow(0 0 100px white)";
-    img.style.transition = "all 2s ease-in"
+    //img.style.transition = "all 2s ease-in"
 
     document.body.appendChild(img);
 }
