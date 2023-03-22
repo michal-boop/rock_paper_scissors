17d21a1e0e245f899c90f5371d60eb00e0990a70
michal-boop <majzermichal@gmail.com>
2023-03-22 14:23:04 +0100

    put mechanics of the game in place

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,6 @@
 let userChoice //create a variable for user's choice of weapon
-let computerChoice //create a variable to store computer's choice of weapon
+let computerChoice//create a variable to store computer's choice of weapon
+
 function getComputerChoice() { //use a function to randomly assign weapon to computer's variable
     let computerWeapon
     let x = Math.ceil(Math.random()*9);
@@ -16,27 +17,78 @@ function getComputerChoice() { //use a function to randomly assign weapon to com
     console.log(computerWeapon)
     return computerWeapon;  
 }
+
 let userScore = 0
 console.log(userScore)
 let computerScore = 0
 console.log(computerScore)
+
 function playRound (userChoice, computerChoice) {
     if (userChoice === computerChoice) { //use conditional statements to determine the winner of a round
-        alert (`You chose ${userChoice}, computer chose ${computerChoice}, it's a tie!`) //print the message announcing the winner and weapons of choice        
+        document.getElementById("message").innerHTML=`You chose ${userChoice}, computer chose ${computerChoice}, it's a tie!`; //print the message announcing the winner and weapons of choice        
     }
     else if ((userChoice === "rock" && computerChoice === "paper")||(userChoice === "paper" && computerChoice === "scissors")||(userChoice === "scissors" && computerChoice === "rock")) {
-        alert (`You chose ${userChoice}, computer chose ${computerChoice}, computer scores!`)
+        document.getElementById("message").innerHTML=`You chose ${userChoice}, Computer chose ${computerChoice}, Computer scores!`;
         computerScore++
-        alert (`User: ${userScore} vs Computer: ${computerScore}`)
     }
-    else if ((userChoice === "rock" && computerChoice === "scissors")||(userChoice === "paper" && computerChoice === "rock")||(userChoice === "scissors" && computerChoice === "paper")) {
-        alert (`You chose ${userChoice}, computer chose ${computerChoice}, user scores!`)
+    else /*if ((userChoice === "rock" && computerChoice === "scissors")||(userChoice === "paper" && computerChoice === "rock")||(userChoice === "scissors" && computerChoice === "paper"))*/{
+        document.getElementById("message").innerHTML=`You chose ${userChoice}, Computer chose ${computerChoice}, User scores!`;
         userScore++
-        alert (`User: ${userScore} vs Computer: ${computerScore}`)
+    }
+    document.getElementById("user_score").innerHTML=`${userScore}`;
+    document.getElementById("computer_score").innerHTML=`${computerScore}`;
+}
+
+const btn = document.querySelectorAll('button');
+btn.forEach((button) => {
+    button.addEventListener('click', () => {
+        userChoice = `${button.id}`;
+        computerChoice = getComputerChoice();
+        playRound(userChoice, computerChoice);
+        checkWin(userScore, computerScore);
+    });
+});
+
+/*const btnRock = document.querySelector('#rock');
+btnRock.addEventListener('click', () => {
+    userChoice = "rock";
+    computerChoice = getComputerChoice();
+    playRound(userChoice, computerChoice);
+    checkWin(userScore, computerScore);
+});
+
+const btnPaper = document.querySelector('#paper');
+btnPaper.addEventListener('click', () => {
+    userChoice = "paper";
+    computerChoice = getComputerChoice();
+    playRound(userChoice, computerChoice);
+    checkWin(userScore, computerScore);
+});
+
+const btnScissors = document.querySelector('#scissors');
+btnScissors.addEventListener('click', () => {
+    userChoice = "scissors";
+    computerChoice = getComputerChoice();
+    playRound(userChoice, computerChoice);
+    checkWin(userScore, computerScore);
+}); */
+
+function checkWin (userScore, computerScore) {
+    if (userScore===5) {
+        document.getElementById("message").innerHTML="User wins!"; //display message for the winner
+        location.reload();
+    }
+    else if (computerScore===5) {
+        document.getElementById("message").innerHTML="Computer wins!";
+        location.reload();
+    } 
+    else {
+        document.getElementById("message").innerHTML="Next round!";
     }
 }
+/*
 function game() {
-    /*while (userScore<5 && computerScore<5)*/ { //create a loop which will keep playing the game until user or computer scores 5 points
+    while (userScore<5 && computerScore<5) { //create a loop which will keep playing the game until user or computer scores 5 points
         userChoice = prompt("Choose your weapon: Rock, Paper or Scissors?", "").toLowerCase();//assign new value to variable holding user's choice of weapon, make it case insensitive
         if (((userChoice!=="rock")&&(userChoice!=="paper")&&(userChoice!=="scissors"))||userChoice===null) {//create a contingency in case of a typo or choice of null allowing another choice without making the loop run needlessly
             alert ("Weapon unavailable, pick again!");
@@ -47,11 +99,11 @@ function game() {
         console.log(computerChoice)
         playRound(userChoice, computerChoice);//play a round    
     }
-    /*if (userScore===5) {
+    if (userScore===5) {
         alert ("User wins!") //display message for the winner
     }
     else if (computerScore===5) {
         alert ("Computer wins!")
-    } */
+    } 
 }
-console.log(game()) //run the game
+console.log(game()) //run the game */
